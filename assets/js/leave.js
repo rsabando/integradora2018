14d261919217b10fd005cc129de1c4fcf850e119
rsabando <rsabando@espol.edu.ec>
2018-12-24 22:15:17 -0500

    modificando frutas

--- assets/js/leave.js
+++ assets/js/leave.js
@@ -8,9 +8,29 @@ var container = document.getElementById("leaves"),	w = window.innerWidth , h = w
 
  for (i=0; i<total; i++){
    var Div = document.createElement('div');
+   var Div2 = document.createElement('div');
+   var Div3 = document.createElement('div');
+   var Div4 = document.createElement('div');
+   var Div5 = document.createElement('div');
+   var Div6 = document.createElement('div');
    TweenLite.set(Div,{attr:{class:'dot'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   TweenLite.set(Div2,{attr:{class:'dot2'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   TweenLite.set(Div3,{attr:{class:'dot3'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   TweenLite.set(Div4,{attr:{class:'dot4'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   TweenLite.set(Div5,{attr:{class:'dot5'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   TweenLite.set(Div6,{attr:{class:'dot6'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
    container.appendChild(Div);
+   container.appendChild(Div2);
+   container.appendChild(Div3);
+   container.appendChild(Div4);
+   container.appendChild(Div5);
+   container.appendChild(Div6);
    animm(Div);
+   animm(Div2);
+   animm(Div3);
+   animm(Div4);
+   animm(Div5);
+   animm(Div6);
  }
 
  function animm(elm){
